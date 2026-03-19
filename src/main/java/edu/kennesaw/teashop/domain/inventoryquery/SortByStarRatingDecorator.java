490cf198cbdec2d0458389b7bb102caecd930c24
copelansam <copelansam@gmail.com>
2026-03-19 14:50:33 -0400

    Create logic for sorting by star rating

--- src/main/java/edu/kennesaw/teashop/domain/inventoryquery/SortByStarRatingDecorator.java
+++ src/main/java/edu/kennesaw/teashop/domain/inventoryquery/SortByStarRatingDecorator.java
@@ -2,18 +2,32 @@ package edu.kennesaw.teashop.domain.inventoryquery;
 
 import edu.kennesaw.teashop.domain.inventory.InventoryItem;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class SortByStarRatingDecorator extends InventoryQueryDecoratorBase {
 
-    private boolean ascending;
+    private final SortDirection direction;
 
-    public SortByStarRatingDecorator(IInventoryQuery query, boolean ascending){
+    public SortByStarRatingDecorator(IInventoryQuery query, SortDirection direction){
         super(query);
-        this.ascending = ascending;
+        this.direction = direction;
     }
 
-    public List<InventoryItem> getItems(){
-        // code to sort the items by star rating
+    public List<QueriedInventoryItem> getItems(){
+
+        // Create a copy of the query that we can sort
+        List<QueriedInventoryItem> sortByRating = new ArrayList<>(wrappedQuery.getItems());
+
+        if (direction == SortDirection.ASCENDING){ // Sort by ascending star ratings
+            sortByRating.sort(Comparator.comparing((QueriedInventoryItem item )-> item.getStarRating().getRating()));
+        }
+        else if (direction == SortDirection.DESCENDING){ // Sort by descending star ratings
+            sortByRating.sort(Comparator.comparing((QueriedInventoryItem item) -> item.getStarRating().getRating()).reversed());
+        }
+
+        return Collections.unmodifiableList(sortByRating);
     }
 }
